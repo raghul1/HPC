fcfea5d222b4873fbac1b62319d4e2d9ee9bf163
Raghul Ravichandran <Raghul@dyn3168-83.wlan.ic.ac.uk>
2018-03-14 09:55:42 +0000

    Basic geometry added

---
+++ cmake-build-debug/CMakeFiles/Case1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Case1.dir/main.cpp.o"
+  "Case1.pdb"
+  "Case1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Case1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
