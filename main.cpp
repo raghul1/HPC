0b02f26b71d741d4dc339f2a0f9b1fa30febb2a1
Raghul Ravichandran <Raghul@dyn3170-169.wlan.ic.ac.uk>
2018-03-15 14:13:41 +0000

    Added nodal coorindates: case 1 translation

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include "cblas.h"
 using namespace std;
 
+// void zeros(int, float*);
+
 int main() {
 
     // -----------------------
@@ -33,14 +35,14 @@ int main() {
     float b = -a * L + (h2 - h1) / L;
 
     //----- Meshing Geometry -----------------
-    int nelem_y = 8;    // Number of elements in y-direction
-    int nelem_x = 15;   // Number of elements in the x-direction
+    const int nelem_y = 5;    // Number of elements in y-direction
+    const int nelem_x = 10;   // Number of elements in the x-direction
 
     int nnode_elem = 4 ;                // number of nodes in each element
-    double nNodeDof[4] = {1, 1, 1, 1} ;    // number of DoF per node (1 = Temperature only)
-    // total number of DoF per element given by the sum the nodal DoF
-    int neDof = cblas_dasum(4, nNodeDof, 1);    // dot product with an array of ones to sum elements of array nNodeDof
-    cout << neDof;
+    //float nNodeDof[4] = {1, 1, 1, 1} ;    // number of DoF per node (1 = Temperature only)
+    // total number of DoF given by the sum of the nodal DoF
+    //int neDof = cblas_sasum(4, nNodeDof, 1);    // CBLAS routine to sum elements of array nNodeDof
+
     //----- Element and node numbers -----------------
     int nelem = nelem_x * nelem_y;              // Total number of elements
     int nnode = (nelem_x + 1) * (nelem_y + 1);   // Total number of nodes
@@ -54,17 +56,70 @@ int main() {
 
     //----- Calculation of Nodal coordinate matrix -> Coord ---------
     // h(x) = a * x**2 + b * x + h1  // Beam height as a function of x
-//                                       x = np.linspace(0, L, nelem_x + 1)
 
-//    h = a * x**2 + b * x + h1
-//    Y = np.zeros((nelem_y + 1, nelem_x + 1))
-//
-//    for colnr in range(nelem_x + 1):
-//    Y[:, colnr] = np.linspace(-h[colnr] / 2, h[colnr] / 2, nelem_y + 1) // create top and bottom position for each x and fill Y with h positions.
-//
-//            Coord = np.zeros((nnode, 2))  // Initialise coordinate of the nodes [x, y]
+    // initialise Y with zeros using zeros function - not required, however kept for consideration
+    float Y[(nelem_x+1) * (nelem_y+1)]; // ** replace Y index with nNode
+    // float* Y_pointer = &(Y[0]);
+    // zeros((nelem_x+1) * (nelem_y+1), Y_pointer);
+
+    // create top and bottom position for each x and fill Y with h positions.
+    // Y is stored as a column major matrix and hence is filled column by column.
+
+    // initialise vector x to store nodal coordinates
+    float x[nelem_x];
+    // linear spacing of node coordinates
+    for (int i = 0; i <= nelem_x; i++){
+
+        x[i] = L / nelem_x * i;
+    }
+
+    // initialise h as the plate upper edge boundary (mirrored for lower boundary)
+    float h[nelem_x];
+    // calculate h for each point in x
+    for (int i = 0; i <= nelem_x; i++) {
+
+        h[i] = a * x[i] * x[i] + b * x[i] + h1;
+
+    }
 
 
+
+
+    for (int colnr = 0; colnr < nelem_x +1; colnr++){
+        for (int rownr = 0; rownr < nelem_y + 1; rownr++){
+
+            Y[colnr * nelem_x + rownr] = -h[colnr] / 2 + h[colnr] / nelem_y * rownr ;
+
+        }
+    }
+
+    // create Coordinate matrix with two rows (x,y) for each node.
+    float Coord[nnode * 2];
+
+    // loop through each x and y value, producing coordinates. Stored as column major {x(:), y(:)}
+    for (int i = 0; i < nelem_x+1; i++){
+        for(int j = 0; j < nelem_y+1; j++){
+
+            Coord[j + i*nelem_y] = x[i];                // first column : x
+            Coord[(j + i*nelem_y) + nnode] = Y[j];      // second column : y
+
+            cout << x[i] << "    " <<  Coord[(j + i*nelem_y) + nnode] << endl;
+        }
+    }
+
+    
 
     return 0;
 }
+/*
+void zeros(int n, float* A){
+
+
+    for (int i = 0; i < n ; i++){
+
+        A[i] = 3;
+        cout << A[i];
+
+    }
+}
+ */
