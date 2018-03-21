a3d59efbb7ce383423c84a83ad0fd4048c26ee37
raghul1 <37346761+raghul1@users.noreply.github.com>
2018-03-21 19:15:40 +0000

    Case 1 translation: added matrix partitions
    Partitioned global stiffness matrix. Must move to the remote desktop in order to complete translation with linear algebra solver / LAPACK.

--- main.cpp
+++ main.cpp
@@ -90,8 +90,6 @@ int main() {
     }
 
 
-
-
     for (int colnr = 0; colnr < nelem_x +1; colnr++){
         for (int rownr = 0; rownr < nelem_y + 1; rownr++){
 
@@ -99,16 +97,17 @@ int main() {
         }
     }
 
-
     // create Coordinate matrix with two rows (x,y) for each node.
     float Coord[nnode * 2];
+    int t = 0;
 
     // loop through each x and y value, producing coordinates. Stored as column major {x(:), y(:)}
     for (int i = 0; i < nelem_x+1; i++){
         for(int j = 0; j < nelem_y+1; j++){
 
-            Coord[j + i*nelem_y] = x[i];                // first column : x
-            Coord[(j + i*nelem_y) + nnode] = Y[j];      // second column : y
+            Coord[j + i*(nelem_y+1)] = x[i];                // first column : x
+            Coord[(j + i*(nelem_y+1)) + nnode] = Y[j];      // second column : y
+
         }
     }
 
@@ -195,6 +194,10 @@ int main() {
         }
 
     }
+
+    // ------------------------
+    // **      K MATRIX     **
+    // ------------------------
 
     // ----- Assembly of global stiffness matrix K ---------------
 
@@ -301,23 +304,233 @@ int main() {
         }
         zeros_double(nnode_elem*nnode_elem, &(Ke[0]));              // remember to zero Ke after each element iteration
 
+    }
 
+
+    // ------------------------
+    // **      CASE 1       **
+    // ------------------------
+
+    // nodes of incoming heat flux. Case 1: right side of beam.
+    int fluxnodes[nelem_y];
+
+    for (int i = 0; i < nelem_y + 1; i++){
+
+        fluxnodes[i] = NodeTopo[(nelem_y+1) * nelem_x + i];
+
+    }
+    int nFluxNodes = nelem_y+1;
+
+    //----- Defining load ----------------------------
+
+    // constant flux at right edge of the beam
+    int q = 2500;
+
+    int n_bc[4 * (nFluxNodes - 1)];
+
+    for (int i = 0; i<nFluxNodes+1; i++){
+
+            n_bc[i*4 ] = fluxnodes[i];           // first row: Node 1
+            n_bc[i*4 + 1] = fluxnodes[i + 1];   // second row: Node 2
+            n_bc[i*4 + 2] = q;                  // third and fourth rows: flux in
+            n_bc[i*4 + 3] = q;
 
     }
 
 
-        for (int t=0;t<nnode_elem;t++){
-        for (int s=0;s<nnode_elem;s++){
-            //cout << Ke[s + t*nnode] << "  ";
+    int nbe = nFluxNodes - 1;                   // number of flux elements
+    double f[nDof];                             // nodal flux vector
+    zeros_double(nDof, &(f[0]));                // zero overall flux vector
+    double fq[2];                               // flux vector for nodes 1 & 2
+    int node1; int node2; double n_bce[2];           // nodes 1 & 2 and flux value at edge
+    float x1; float y1; float x2; float y2;            // nodal coordinates
+    double leng;
+    double flux[1];                             // elemental flux as an array length 1 (for CBLAS)
+    double N_1d[2];                             // shape function matrix
+
+    for (int i = 0; i<nbe; i++){
+
+        // node number from boundary condition matrix, for each element edge
+        node1 = n_bc[i*4];
+        node2 = n_bc[i*4+1];
+        n_bce[0] = n_bc[i*4+2];
+        n_bce[1] = n_bc[i*4+3];
+
+        // nodal coordinates of element edge into separate variables - easily removed
+
+        x1 = Coord[node1];
+        y1 = Coord[node1 + nnode];
+        x2 = Coord[node2];
+        y2 = Coord[node2 + nnode];
+
+        leng = sqrt(    pow((x1 - x2), 2) + pow((y1 - y2), 2)   );      // edge length
+
+        detJ = leng/2;              // 1D Jacobian
+
+        zeros_double(2, &(fq[0]));  // reset fq[] to zero
+
+        // integrate in xi direction (1D)
+        for(int j = 0; j < gaussorder; j++){
+
+            xi = GP[j];     // 1D shape function
+
+            N_1d[0] = 0.5 * (1 - xi);   // populate shape function matrix
+            N_1d[1] = 0.5 * (1 + xi);
+
+
+            // flux = N_1d . n_bce
+            cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, 1, 1, 2, 1.0, N_1d, 1, n_bce, 2, 0.0, flux, 1);
+
+            // nodal flux
+            fq[0] = (fq[0] + W[j] * N_1d[0] * flux[0] * detJ * th);
+            fq[1] = (fq[1] + W[j] * N_1d[1] * flux[0] * detJ * th);
+
+
+
         }
-        cout << endl;
+
+        f[node1] -= fq[0];      // subtract to make integral negative
+        f[node2] -= fq[1];
+
     }
-    for (int t=0;t<nnode;t++){
-        for (int s=0;s<nnode;s++){
-            cout << K[s*nnode + t] << setw(10);
+
+
+    // ---------------------------
+    // **  BOUNDARY CONDITIONS  **
+    // ---------------------------
+
+
+    // ----- Apply boundary conditions ----------------- Essential B.C.
+    int TempNodes[nelem_y];
+
+    // nodes at the left of beam (temp held constant here)
+    for (int i = 0; i < nelem_y + 1; i++){
+        TempNodes[i] = NodeTopo[i];
+    }
+
+    // number of nodes with temp BC
+    int nTempNodes = nelem_y + 1;
+
+    // initialize the nodal temperature vector
+    float BC[nTempNodes * 2];
+
+    // boundary temperature
+    float T0 = 10.0;
+
+    // create array with nodes and corresponding temp
+    for (int i = 0; i < nTempNodes; i++){
+        BC[i] = NodeTopo[i];
+        BC[i + nTempNodes] = T0;
+    }
+
+    // ----- Assembling global "Force" vector ------------
+    // original Dof and temperature vector
+    int OrgDof[nDof];
+    zeros_int(nDof, &(OrgDof[0]));
+    double T[nDof];
+    zeros_double(nDof, &(T[0]));
+    int rDof = nDof;    // reduced number of Dof
+
+    for (int i = 0; i < nTempNodes; i++){
+
+        OrgDof[TempNodes[i]] = - 1;
+        T[TempNodes[i]] = BC[i + nTempNodes];
+
+    }
+    // update reduced degrees of freedom
+    rDof -= nTempNodes;
+
+    int RedDof[rDof];
+    int counter1 = 0;
+    zeros_int(rDof, &(RedDof[0]));
+
+    for (int i = 0; i < nDof; i++){
+
+        if (OrgDof[i] == 0){
+            OrgDof[i] = counter1;
+            RedDof[counter1] = i;
+            counter1++;
         }
-        cout << endl;
     }
+
+    // ---------------------------
+    // **  PARTITION MATRICES  **
+    // ---------------------------
+
+    int trueE = nTempNodes;            // size of true values of mask_E, used later for size of matrix partitions
+    int falseE = nDof - nTempNodes;  // size of false values of mask_E
+    double T_E[trueE];
+    double f_F[falseE];
+    int T_Ecount = 0;
+    int f_Fcount = 0;
+    double K_EE[trueE * trueE];
+    double K_FF[falseE * falseE];
+    double K_EF[trueE * falseE];
+
+
+    // known temp Dof
+    // boolean mask - mask_E
+    bool mask_E[nDof];
+    for(int i = 0; i < nDof; i++){
+        mask_E[i] = false;               // initialise each entry as false
+
+        for(int j = 0; j < nTempNodes; j++){
+            if (i == TempNodes[j]){
+                mask_E[i] = true;       // loop through TempNodes and return true if any value 1:nDof matches
+            }
+        }
+
+        if(mask_E[i] == 1){
+            T_E[T_Ecount] = T[i];
+            T_Ecount++;
+        }
+        else {
+            f_F[f_Fcount] = f[i];
+            f_Fcount++;
+        }
+
+    }
+
+    // ----- populating partitioned matrices ------------------
+    // set size based on mask_E. Loop though partition and insert into relevant row (j) and column (i) from K
+    // since global stiffness may be split into consecutive 'chunks'
+
+    // K_EE -- python: K_EE = K[np.ix_(mask_E, mask_E)]
+    for (int i = 0; i < trueE; i++) {
+        for (int j = 0; j < trueE; j++) {
+
+            K_EE[i * trueE + j] =  K[i * nDof + j];
+
+        }
+    }
+
+    // K_FF = K[np.ix_(~mask_E, ~mask_E)]
+    for (int i = 0; i < falseE; i++) {
+        for (int j = 0; j < falseE; j++) {
+
+            K_FF[i * falseE + j] =  K[(i+trueE) * nDof + (j+trueE)];
+
+        }
+    }
+
+
+    // K_EF = K[np.ix_(mask_E, ~mask_E)]
+    for (int i = 0; i < falseE; i++) {
+        for (int j = 0; j < trueE; j++) {
+
+            K_EF[i * trueE + j] =  K[(i+trueE) * nDof + (j)];
+
+        }
+    }
+
+
+    // ---------------------------
+    // **  CONSTRUCT SOLUTION  **
+    // ---------------------------
+
+
+    
+
 
 
 
